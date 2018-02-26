794fd3c1027471af35577e8e348ff5b0e3341111
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-26 17:02:19 +0000

    refactor assign c/d transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -6,6 +6,10 @@ class Transaction
     @amount = amount
     @credit = 0
     @debit = 0
+    assign_credit_or_debit(amount)
+  end
+
+  def assign_credit_or_debit(amount)
     (amount > 0) ? @credit = amount : @debit = amount
   end
 end
