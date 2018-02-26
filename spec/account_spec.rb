e912d1d32af9f8d22f972c5359b3cd6d90cc68ff
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-26 16:19:05 +0000

    refactor account spec

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,9 +2,11 @@ require 'account'
 
 describe Account do
 
+  subject(:account) {described_class.new()}
+    let(:transaction) { double :transaction }
+
   describe 'open account' do
     it 'has a balance of zero on creation' do
-      account = Account.new(0)
       expect(account.balance).to equal 0
     end
 
@@ -16,32 +18,25 @@ describe Account do
 
   describe 'deposit and withdraw funds' do
     before do
+      account.credit(30)
     end
 
     it 'receives a deposit and increases balance accordingly' do
-      account = Account.new()
-      account.credit(30)
       expect(account.balance).to equal 30
     end
 
     it 'accepts a withdrawal and decreases balance accordingly' do
-      account = Account.new()
-      account.credit(30)
       account.debit(12)
       expect(account.balance).to equal 18
     end
 
     it 'stores a transaction' do
-      account = Account.new()
-      account.credit(30)
-      expect(account.transactions).to include({credit: 30, balance: 30})
+      expect(account.transactions.length).to eq 1
     end
 
-    it 'stores a transaction' do
-      account = Account.new()
-      account.credit(30)
+    it 'stores more than one transaction' do
       account.debit(12)
-      expect(account.transactions).to include({debit: 12, balance: 18})
+      expect(account.transactions.length).to eq 2
     end
   end
 
