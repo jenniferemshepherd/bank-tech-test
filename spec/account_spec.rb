9e6b7097cd161dca638f5b0255ece1a7515de74f
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-26 15:38:36 +0000

    add deposit method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,25 @@
 require 'account'
 
 describe Account do
-  it 'has a balance of zero on creation' do
-    account = Account.new(0)
-    expect(account.balance).to equal 0
+
+  describe 'open account' do
+    it 'has a balance of zero on creation' do
+      account = Account.new(0)
+      expect(account.balance).to equal 0
+    end
+
+    it 'can be created with a non-zero balance' do
+      account = Account.new(40)
+      expect(account.balance).to equal 40
+    end
   end
+
+  describe 'deposit and withdraw funds' do
+    it 'receives a deposit and increases balance accordingly' do
+      account = Account.new()
+      account.deposit(30)
+      expect(account.balance).to equal 30
+    end
+  end
+
 end
